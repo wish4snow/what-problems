e9e3c2d0709b937e53377ecf72468d8ffbbe804e
wish4snow <157080837+wish4snow@users.noreply.github.com>
2024-01-31 09:29:15 -0600

    added new function

--- problem_lister.cpp
+++ problem_lister.cpp
@@ -31,19 +31,38 @@ vector<int> numbersInRange (string range) {
 
 	for (int i = stoi(range.substr(0, intSplit)); i < stoi(range.substr(intSplit + 1, range.length() - intSplit)) + 1; i++) {
 		array.push_back(i);
+
 	} //end of for loop
 
 	return array;
+} //end of function
+
+vector<int> vectorCombineAndSort (vector<vector<int> > allVectors) {
+	vector<int> finalVector;
+
+	for (int i = 0; i < allVectors.size(); i++) {
+		for (int j = 0; i < allVectors[i].size(); j++) {
+			finalVector.push_back(allVectors[i][j]);
+		}
+
+	}
+
+	return finalVector;
 }
 
 
 int main () {
-	vector<int> poop = numbersInRange("1-16");
-	cout << problemLister("M10") << endl;
-	for (int i = 0; i < poop.size(); i++) {
-		cout << poop[i] << " ";
-	}
-
+	vector<int> poop = numbersInRange("32-35");
+	vector<int> poop2 = numbersInRange("12-14");
+	vector<vector<int> > poop4;
+	poop4.push_back(poop);
+	poop4.push_back(poop2);
+	// vector<int> poop3 = vectorCombineAndSort(poop4);
+	// cout << problemLister("M10") << endl;
+	// for (int i = 0; i < poop3.size(); i++) {
+	// 	cout << poop3[i] << " ";
+	// }
+	// cout << endl;
 
 	return 0;
 }
