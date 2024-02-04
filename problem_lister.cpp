5cddce6a5171a58051188c85b60b54a89448d504
wish4snow <157080837+wish4snow@users.noreply.github.com>
2024-02-04 01:01:31 -0600

    added test file, as well as finishing coding

--- problem_lister.cpp
+++ problem_lister.cpp
@@ -4,13 +4,7 @@
 using namespace std;
 
 
-string problemLister (string problems) {
-	string assignmentLetter = problems.substr(0,1);
 
-
-
-	return assignmentLetter;
-}
 
 vector<int> numbersInRange (string range) {
 	vector<int> array;
@@ -57,21 +51,93 @@ vector<int> vectorCombineAndSort (vector<vector<int> > allVectors) {
 	return finalFinalVector;
 }
 
+string removeSpaces(string text) {
+	string finalText;
 
-int main () {
-	vector<int> test = numbersInRange("32-35");
-	vector<int> test2 = numbersInRange("12-14");
-	vector<int> test5 = numbersInRange("34-40");
-	vector<vector<int> > test4;
-	test4.push_back(test);
-	test4.push_back(test2);
-	test4.push_back(test5);
-	vector<int> test3 = vectorCombineAndSort(test4);
-	cout << problemLister("M10") << endl;
-	for (int i = 0; i < test3.size(); i++) {
-		cout << test3[i] << " ";
+	for (int i = 0; i < text.size(); i++) {
+		if (text[i] != ' ') {
+			finalText +=text[i];
+		}
 	}
-	cout << endl;
 
-	return 0;
+	return finalText;
 }
+
+vector<string> commaSeparator (string text) {
+	string fText = removeSpaces(text);
+	string substring;
+	int commaIndex = 0;
+	vector<string> finalVector;
+
+	for (int i = 1; i < fText.size(); i++) {
+		if (fText[i] != ',') {
+			substring += fText[i];
+
+		} else {
+			finalVector.push_back(substring);
+			substring = "";
+			commaIndex = i;
+
+		}
+	}
+	finalVector.push_back(fText.substr(commaIndex + 1, fText.size()));
+
+	return finalVector;
+}
+
+void problemLister (string problems) {
+	string assignmentLetter = problems.substr(0,1);
+	vector<string> separatedCommas;
+	vector<vector<int> > allRanges;
+	vector<int> sortedNumbers;
+	separatedCommas = commaSeparator(problems);
+	
+	for (int i = 0; i < separatedCommas.size(); i++) {
+		vector<int> ranges = numbersInRange(separatedCommas[i]);
+		allRanges.push_back(ranges);
+	}
+
+	sortedNumbers = vectorCombineAndSort(allRanges);
+
+	cout << "Do problem";
+
+	if (sortedNumbers.size() > 1) {
+
+		cout << "s ";
+		for (int i = 0; i < sortedNumbers.size() - 1; i++) {
+			cout << sortedNumbers[i] << ", ";
+			if (i % 5 == 0 && i > 0) {
+				cout << "\n";
+			}
+		}
+		cout << "and " << sortedNumbers[sortedNumbers.size() - 1];
+		
+	} else {
+		cout << " " << sortedNumbers[0];
+	}
+	cout << " of " << assignmentLetter;
+	cout << endl;
+}
+
+// int main () {
+// 	vector<int> test = numbersInRange("32-35");
+// 	vector<int> test2 = numbersInRange("32-35");
+// 	vector<int> test5 = numbersInRange("32-35");
+// 	vector<vector<int> > test4;
+// 	test4.push_back(test);
+// 	test4.push_back(test2);
+// 	test4.push_back(test5);
+// 	vector<int> test3 = vectorCombineAndSort(test4);
+// 	//cout << problemLister("M10") << endl;
+// 	for (int i = 0; i < test3.size(); i++) {
+// 		cout << test3[i] << " ";
+// 	}
+// 	cout << endl;
+// 	cout << removeSpaces("h e l l o") << endl;
+
+// 	vector<string> s = commaSeparator("T4-5");
+// 	cout << s[0] << endl;
+
+// 	problemLister("T 4 - 5 ,1- 3,7 - 10,8-   12");
+// 	return 0;
+// }
---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "problem_lister.cpp"
+
+int main () {
+	problemLister("L6");
+	problemLister("M1-3");
+	problemLister("N1,2,5");
+	problemLister("P1-3,5");
+	problemLister("Q1-3,5-7");
+	problemLister("R1-1,3-3,5-8");
+	problemLister("T 4 - 5 ,1- 3,7 - 10,8-   12");
+	return 0;
+}
