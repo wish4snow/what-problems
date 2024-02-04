7f7cc7889588786e7bb2a83a3d817cfb748d4a8d
wish4snow <157080837+wish4snow@users.noreply.github.com>
2024-02-03 23:13:52 -0600

    fixed broken code

--- problem_lister.cpp
+++ problem_lister.cpp
@@ -39,30 +39,39 @@ vector<int> numbersInRange (string range) {
 
 vector<int> vectorCombineAndSort (vector<vector<int> > allVectors) {
 	vector<int> finalVector;
-
+	vector<int> finalFinalVector;
 	for (int i = 0; i < allVectors.size(); i++) {
-		for (int j = 0; i < allVectors[i].size(); j++) {
+		for (int j = 0; j < allVectors[i].size(); j++) {
 			finalVector.push_back(allVectors[i][j]);
 		}
 
 	}
 
-	return finalVector;
+	sort(finalVector.begin(), finalVector.end());
+	
+	for (int i = 0; i < finalVector.size(); i++) {
+		if (finalVector[i] != finalVector[i + 1]) {
+			finalFinalVector.push_back(finalVector[i]);
+		}
+	}
+	return finalFinalVector;
 }
 
 
 int main () {
-	vector<int> poop = numbersInRange("32-35");
-	vector<int> poop2 = numbersInRange("12-14");
-	vector<vector<int> > poop4;
-	poop4.push_back(poop);
-	poop4.push_back(poop2);
-	// vector<int> poop3 = vectorCombineAndSort(poop4);
-	// cout << problemLister("M10") << endl;
-	// for (int i = 0; i < poop3.size(); i++) {
-	// 	cout << poop3[i] << " ";
-	// }
-	// cout << endl;
+	vector<int> test = numbersInRange("32-35");
+	vector<int> test2 = numbersInRange("12-14");
+	vector<int> test5 = numbersInRange("34-40");
+	vector<vector<int> > test4;
+	test4.push_back(test);
+	test4.push_back(test2);
+	test4.push_back(test5);
+	vector<int> test3 = vectorCombineAndSort(test4);
+	cout << problemLister("M10") << endl;
+	for (int i = 0; i < test3.size(); i++) {
+		cout << test3[i] << " ";
+	}
+	cout << endl;
 
 	return 0;
 }
